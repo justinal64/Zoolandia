de342ee66f7f745c891caf52c3d82b13283404c3
justin leggett <justin@codingrookie.com>
2017-01-16 10:23:40 -0600

    adding boilerplate code

---
+++ Animals/Animal.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Zoolandia
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        public static void sleep() 
+        {
+            Console.WriteLine("Animal is sleeping");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Zoolandia
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var Lion = new Animal();
+            Lion.Name = "Scarry";
+            Console.WriteLine(Lion.Name); 
+
+            // Console.WriteLine($"I have a Squirrel named {Lion.Name} {Lion.Name} {Lion.Name} {Lion.Name}");
+        }
+    }
+}
